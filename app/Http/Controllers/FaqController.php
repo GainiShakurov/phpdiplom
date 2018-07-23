ba6b852909ae63329d9d117667c617547e95545a
Гайни <shakurovga@gmail.com>
2018-07-23 21:08:09 +0600

    фикс

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -6,6 +6,7 @@ use App\Models\Answer;
 use App\Models\Category;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FaqController extends Controller
 {
@@ -38,9 +39,24 @@ class FaqController extends Controller
     public function postAdd(Request $request)
     {
         $question = new Question();
+
+        $messages = [
+            'inputEmail.required' => 'Введите email!',
+            'inputName.required' => 'Введите имя!',
+            'textQuestion.required' => 'Введите текст вопроса!',
+            'inputEmail.email' => 'Введите email правильно!',
+        ];
+
+        $this->validate($request, [
+            'textQuestion' => 'required|max:1000',
+            'inputName' => 'required',
+            'inputEmail' => 'required|email',
+        ], $messages);
+
         $question->question = $request->input('textQuestion');
         $question->category_id = $request->input('inputCategory');
         $question->author = $request->input('inputName');
+        $question->author_email = $request->input('inputEmail');
 
         $question->save();
 
--- readme.md
+++ readme.md
@@ -8,3 +8,6 @@ http://phpdiplom - клиентская часть
 - composer install
 - php artisan key:generate
 - php artisan migrate
+
+Схема БД
+https://docs.google.com/presentation/d/1yBNoObIqzcstW_2MK8b_F4MP7F7qazZcj1W7vN156Ro/
